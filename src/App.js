c4cf1b8447f43538ebdd8bd7c34eeab626646c40
Chema Torres <chemaaa94@gmail.com>
2022-04-13 15:58:58 -0500

    learning about components ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App(){
-
   return (
   <div>
     <h2>FUNCIONA</h2>
-    <p>This is also visible!</p>
+    <ExpenseItem></ExpenseItem>
     </div>
-  )
+  );
 }
 
 export default App;
