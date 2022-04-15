21e0eec6d094b8f0e8bbab6b4a06bc5d142f2396
Chema Torres <chemaaa94@gmail.com>
2022-04-15 11:17:03 -0500

    Learning Card

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,10 +1,11 @@
+import Card from "./Card";
 import "./Expense.css";
 import ExpenseItem from "./ExpenseItem";
 
 function Expense(props) {
   
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -25,7 +26,7 @@ function Expense(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
