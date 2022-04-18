078a60622fbeea279d1b0d8fc05e42bb03784f3f
Chema Torres <chemaaa94@gmail.com>
2022-04-17 19:41:03 -0500

    New File component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -17,7 +17,7 @@ import "./ExpenseItem.css";
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler} >change title</button>
+      <button onClick={clickHandler}>change title</button>
     </Card>
   );
 }
