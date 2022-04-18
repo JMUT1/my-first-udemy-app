aa353d2bdb673e08cfd152c3dfb74d64ee3bcc9d
Chema Torres <chemaaa94@gmail.com>
2022-04-17 19:57:09 -0500

    Make form work in localhost

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expense from "./components/Expenses/Expense";
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -26,6 +27,7 @@ const App = () => {
   
   return (
     <div>
+      <NewExpense/>
       <Expense items={expenses}/>
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,16 +1,30 @@
-import "./ExpenseForm.css"
+import "./ExpenseForm.css";
 import React from "react";
 
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__controls">
+          <label>Title</label>
+          <input type="text" />
+        </div>
 
-const ExpenseForm = () =>{
-   return <form>
-       <div className="new-expense__controls">
-           <div className="new-expense__controls">
-               <label>Title</label>
-               <input type="text"/>
-           </div>
-       </div>
-   </form>
-}
+        <div className="new-expense__controls">
+          <label>Amout</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+
+        <div className="new-expense__controls">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2023-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
 
 export default ExpenseForm;
